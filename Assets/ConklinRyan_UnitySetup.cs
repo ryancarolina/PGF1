10f37460f3b8ff81a8d2d535411fabbd2729fd73
Ryan Conklin <ryancarolina88@gmail.com>
2019-11-27 14:56:05 -0700

    Unity setup

---
+++ Assets/ConklinRyan_UnitySetup.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConklinRyan_UnitySetup : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello PGF!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
