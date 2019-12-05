5be24f861bfbb4a4c9108cdcef0b0657f13abd4e
Ryan Conklin <ryancarolina88@gmail.com>
2019-12-05 15:28:16 -0700

    week two project script

---
+++ Assets/ConklinRyan_Week2Project.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ConklinRyan_Week2Project : MonoBehaviour
+{
+    Text screen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject go = GameObject.Find("MainText");
+
+        if (go)
+        {
+            screen = go.GetComponent<Text>();
+
+            if (!screen)
+            {
+                Debug.LogError("Text component was not found on MainText");
+            }
+        }
+        else
+        {
+            Debug.LogError("GameObject MainText not found!!");
+        }
+
+        screen.text = "Hello World!";
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
